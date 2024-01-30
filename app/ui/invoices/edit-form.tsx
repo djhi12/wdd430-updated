522299210fa3fdf51f05614a2dd8cedd0088f99a
djhi12 <ido20002@byui.edu>
2024-01-30 14:44:23 +0800

    Initial commit

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,4 +1,6 @@
 'use client';
+import { useForm } from 'react-hook-form';
+import { useFormState } from 'react-dom';
 import { updateInvoice } from '@/app/lib/actions';
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
@@ -11,6 +13,7 @@ import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 // import { updateInvoice } from '@/app/lib/actions';
 
+
 export default function EditInvoiceForm({
   invoice,
   customers,
@@ -18,10 +21,12 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
   }) {
+    const initialState = { message: null, errors: {} };
     const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+    const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
     // const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
-    <form action={updateInvoiceWithId}>
+    <form action={dispatch}>
     <input type="hidden" name="id" value={invoice.id} />
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
